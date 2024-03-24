584b444fa4f2b5e46a2e21a9ac66d7311fdecbce
grisini <110596512+grisini@users.noreply.github.com>
2024-03-24 21:58:10 +0100

    Add files via upload

---
+++ nepremicnine.bat
@@ -0,0 +1,23 @@
+@echo off
+start chrome --new-window https://e-uprava.gov.si/si/e-uprava/oglasnadeska.html?rijs=2316#eyJmaWx0ZXJzIjp7InBlcmlvZGEiOlsiLSJdLCJ0eXBlIjpbIlZTRSIsIlBLWiJdLCJyaWpzIjpbIjIzMTYiXSwib2Zmc2V0IjpbIjAiXSwic2VudGluZWxfdHlwZSI6WyJvayJdLCJzZW50aW5lbF9zdGF0dXMiOlsib2siXSwiaXNfYWpheCI6WyIxIl19LCJvZmZzZXRQYWdlIjpbNDMxXX0=
+timeout /t 1 /nobreak > NUL
+start chrome https://e-uprava.gov.si/si/e-uprava/oglasnadeska.html?rijs=2316#eyJmaWx0ZXJzIjp7InBlcmlvZGEiOlsiLSJdLCJ0eXBlIjpbIlZTRSIsIlBLWiJdLCJyaWpzIjpbIjIzMTUiXSwib2Zmc2V0IjpbIjAiXSwic2VudGluZWxfdHlwZSI6WyJvayJdLCJzZW50aW5lbF9zdGF0dXMiOlsib2siXSwiaXNfYWpheCI6WyIxIl19LCJvZmZzZXRQYWdlIjpbMTA2XX0=
+timeout /t 1 /nobreak > NUL
+start chrome https://sodnedrazbe.si/?subjectType=042047c4-98c8-48dd-bd3c-604ce080de5d,e9d8c86f-9920-461b-aaf1-81fa19162bd2
+timeout /t 1 /nobreak > NUL
+start chrome https://edrazbe.si/list
+timeout /t 1 /nobreak > NUL
+start chrome https://sodnedrazbe.si/?court=4a818d83-2ae4-48a0-8029-ad2490eeb1a8,9ea7fe1e-a42b-4cee-abc0-b7476b11cab3
+timeout /t 1 /nobreak > NUL
+start chrome https://sodnedrazbe.si/?subjectType=042047c4-98c8-48dd-bd3c-604ce080de5d,a6e91ae0-febf-4a62-bce4-fb734c93f766
+timeout /t 1 /nobreak > NUL
+start chrome https://www.nepremicnine.net/oglasi-prodaja/podravska/maribor/mb-center/stanovanje/?s=16
+timeout /t 1 /nobreak > NUL
+start chrome https://www.bolha.com/prodaja-stanovanja/maribor
+timeout /t 1 /nobreak > NUL
+start chrome https://www.bolha.com/prodaja-stanovanja/slovenska-bistrica
+timeout /t 1 /nobreak > NUL
+start chrome https://nepremicnine.sdh.si/prodaja/prodaja.k1/stanovanje.v1/Slovenija.d194/
+timeout /t 1 /nobreak > NUL
+start chrome https://nepremicnine.sdh.si/prodaja/prodaja.k1/pocitniski-objekt.v9/
+timeout /t 1 /nobreak > NUL
